f3c144c071c1ad7f24153fb6ec101089da84374d
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2023-06-18 09:56:24 +0700

    UI Design

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import AudioInput from '@/src/components/audioInput';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>LOZ</h1>
-      <AudioInput />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 wrapper">
+      <div className='App-card mt-8'>
+      <div className='App-card-Item'>
+        <h1 className="font-bold text-lg App-title">Emotion recognition through sound</h1>
+        <AudioInput />
+      </div>
+      </div>
     </main>
   )
 }
--- src/components/audioInput.tsx
+++ src/components/audioInput.tsx
@@ -3,41 +3,63 @@
 import { useState } from "react"
 import AudioUploader from "./audioExtraction";
 import { fetchEmotion } from "../util/fetchEmotion";
-
+import { ChangeEventHandler } from 'react';
 export default function AudioInput() {
     const [audioData, setAudioData] = useState<Float32Array | null>(null);
     const [rate, setRate] = useState(48000);
+    const [model, setModel] = useState('');
+    const [emo, setEmo] = useState('');
+
     const handleAudioDataExtracted = (audioData: Float32Array, sampleRate: number) => {
         setAudioData(audioData);
         setRate(sampleRate);
       };
-
     const handleClick = async () => {
         if (audioData === null) {
-            console.log('Please uploading an audio first');
+            alert('Please uploading an audio first');
         }
         else {
             const audio_array = Array.from(audioData);
             const data = {
-                method: 'LSTM',
+                method: model,
                 rate: rate,
                 numeric: audio_array
             }
             console.log('Send data', JSON.stringify(data));
             const result = await fetchEmotion(JSON.stringify(data));
             console.log('Result:', result);
+            setEmo(result.result);
+            
         }
     }
-    
+    const HandleRadioChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setModel(e.target.value);
+        console.log(model);
+    }
     console.log(audioData);
     console.log(rate);
 
     return (
         <div>
             <AudioUploader onAudioDataExtracted={handleAudioDataExtracted} />
-            <button onClick={handleClick}>
-                Click me
+            <h3 className="mt-4 select-title">Please select the model you want:</h3>
+            <div className="m-4">
+                <input type="radio" id="svm" name="model" value="SVM" onChange={HandleRadioChange}  />
+                <label htmlFor="svm">SVM</label>
+            </div>
+            <div className="m-4">
+                <input type="radio" id="randomforest" value="RF" name="model" onChange={HandleRadioChange}/>
+                <label htmlFor="randomforest" >Random Forest</label>
+            </div>
+            <div className="m-4">
+                <input type="radio" id="lstm" value="LSTM" name="model" onChange={HandleRadioChange}/>
+                <label htmlFor="lstm">LSTM</label>
+            </div>
+            <br />
+            <button onClick={handleClick} className="Item-button">
+                Predict
             </button>
+            {emo && <div>Emotions: {emo}</div>}
         </div>
     )
 }
